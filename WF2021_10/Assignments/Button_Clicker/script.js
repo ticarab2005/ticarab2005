1f03615aba14b00fae85609b9750c836a1ed29c8
Ticara Bell <90620635+ticarab2005@users.noreply.github.com>
2021-11-30 19:01:19 -0600

    Add files via upload

---
+++ WF2021_10/Assignments/Button_Clicker/script.js
@@ -0,0 +1,16 @@
+function logout(element) {
+        element.innerText = "Logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function alertName() {
+    alert("Ninja was Liked!");
+}
+
+
+function liked(element) {
+    element.innerText = "liked";
+}
